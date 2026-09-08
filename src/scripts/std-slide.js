bdba739021f046b208c9f3850ca396670fecdfe2
Aji Sasmito <revbyaji@gmail.com>
2026-09-08 17:50:08 +0700

    Adjust autoSlideX parameters for stdSlide

--- src/scripts/std-slide.js
+++ src/scripts/std-slide.js
@@ -1,5 +1,5 @@
 import { autoSlideX } from './slide-helper.js';
 
 export function initStdSlide() {
-  autoSlideX('stdTrack', '.std-bg', { intervalMs: 5000, transitionMs: 2200, resetDelayMs: 2300 });
+  autoSlideX('stdTrack', '.std-bg', { intervalMs: 5500, transitionMs: 2500, resetDelayMs: 2600 });
 }
